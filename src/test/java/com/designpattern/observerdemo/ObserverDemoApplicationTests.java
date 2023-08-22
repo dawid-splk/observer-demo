032432f49df57d2fcd992941acfadc9a2e869033
DSpalek <dawid.t.spalek@gmail.com>
2023-08-22 15:43:27 +0200

    initial commit

---
+++ src/test/java/com/designpattern/observerdemo/ObserverDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designpattern.observerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObserverDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
